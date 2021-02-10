db1983f439268d2919cb5d95673c162228c20464
Gaurab-Sarkar-au8 <64783626+Gaurab-Sarkar-au8@users.noreply.github.com>
2021-02-10 17:44:44 +0530

    let const var

--- complete-javascript-course-master/01-Fundamentals-Part-1/starter/script.js
+++ complete-javascript-course-master/01-Fundamentals-Part-1/starter/script.js
@@ -24,7 +24,7 @@ let mySecondJob = 'teacher';
 */
 
 //DATA TYPES-----------------------------------------------------------------------------
-let javascriptIsFun = true;
+/*let javascriptIsFun = true;
 console.log(javascriptIsFun);
 
 console.log(typeof javascriptIsFun);
@@ -43,3 +43,24 @@ console.log(typeof year);
  console.log(typeof year);
 
  console.log(typeof null);//error in typeof: it shows type of null is object
+*/
+
+//LET, CONST AND VAR----------------------------------------------------------------------------
+// let and const were introduced in es6, var is used in old javascript
+
+let age = 30;
+age = 31; // muable variable, won't giv error
+
+const birthYear = 1991;
+birthYear = 1990; //this will give error because variable declared with const won't mutate i.e immutable
+const job;//this will also give error, it should have a value
+
+// best practice in coding is that to use const everywhere and let should only be used when the value of variable needs to be changed in some point of program, it reduces the bugs in a program
+
+var job = 'programmer';
+job = 'teacher';
+//though var and let seems same they are not, let is blockscoped and var is finction scoped
+
+lastName = 'Sarkar';
+console.log(lastName);//return Sarkar as result, though it works its actually wrong, it doesnot create variable in the current scope, javascript will create a property in global object
+
