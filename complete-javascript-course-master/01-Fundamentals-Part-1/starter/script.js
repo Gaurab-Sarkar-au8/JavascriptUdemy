5b3b09d44cda275581dc8aff7f6b341dd3213b5e
Gaurab-Sarkar-au8 <64783626+Gaurab-Sarkar-au8@users.noreply.github.com>
2021-02-10 17:01:23 +0530

    les 2

--- complete-javascript-course-master/01-Fundamentals-Part-1/starter/script.js
+++ complete-javascript-course-master/01-Fundamentals-Part-1/starter/script.js
@@ -1,3 +1,45 @@
+/*
+//VALUES AND VARIABLES------------------------------------------------------------------
+
 let js = "amazing";
-if (js === "amazing") alert("Javascript is FUN!");
 console.log(10+20+40-20);
+
+console.log("Jonas")
+
+let firstName = "Matilda";
+
+console.log(firstName)
+console.log(firstName)
+console.log(firstName)
+
+// variable name conventions
+let jonas_matilda = "JM";
+let $function = 27;
+
+let person = "jonas";
+let PI = 3.1415;
+
+let myFirstJob = 'programmer';
+let mySecondJob = 'teacher';
+*/
+
+//DATA TYPES-----------------------------------------------------------------------------
+let javascriptIsFun = true;
+console.log(javascriptIsFun);
+
+console.log(typeof javascriptIsFun);
+console.log(typeof true);
+console.log(typeof 23);
+console.log(typeof 'jonas');
+
+javascriptIsFun = 'YES!'; //dynamic typing
+console.log(typeof javascriptIsFun);
+
+let year;
+console.log(year);
+console.log(typeof year);
+
+ year = 1991;
+ console.log(typeof year);
+
+ console.log(typeof null);//error in typeof: it shows type of null is object
