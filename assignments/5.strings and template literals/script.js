6349c4cf69508ef10e074eb4df6f8489d004f307
Gaurab-Sarkar-au8 <64783626+Gaurab-Sarkar-au8@users.noreply.github.com>
2021-02-12 16:45:15 +0530

    no.5

---
+++ assignments/5.strings and template literals/script.js
@@ -0,0 +1,6 @@
+country = `India`;
+continent = `Asia`;
+population = 100;
+language = `Hindi`;
+const description = `${country} is in ${continent}, and its ${population} million people speak ${language}`;
+console.log(description);
