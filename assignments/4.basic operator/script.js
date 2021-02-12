2216c5ec1f2928b21627822c90f80850ecbc0c1d
Gaurab-Sarkar-au8 <64783626+Gaurab-Sarkar-au8@users.noreply.github.com>
2021-02-12 13:21:44 +0530

    no.4

---
+++ assignments/4.basic operator/script.js
@@ -0,0 +1,14 @@
+let countryPopulation = 100;
+countryPopulation++;
+console.log(countryPopulation);
+let halfPoplation = countryPopulation/2;
+
+let finalandPopulation = 6;
+console.log(countryPopulation > finalandPopulation);
+
+let averagePopulation = 33;
+console.log(countryPopulation < averagePopulation);
+
+const description = 'Portugal is in Europe, and its 11 million people speak portuguese';
+console.log(description);
+
