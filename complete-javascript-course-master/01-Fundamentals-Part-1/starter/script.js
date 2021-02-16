33d6b2e337810f258eba0da5c718c19ba63e9405
Gaurab-Sarkar-au8 <64783626+Gaurab-Sarkar-au8@users.noreply.github.com>
2021-02-16 16:24:30 +0530

    logical operators

--- complete-javascript-course-master/01-Fundamentals-Part-1/starter/script.js
+++ complete-javascript-course-master/01-Fundamentals-Part-1/starter/script.js
@@ -221,7 +221,7 @@ if (height) {
 */
 
 //EQUALITY OPERATORS: == VS === -------------------------------------------------------------------
-/**/
+/*
 const age = '18';
 //=== comparision operator
 //=== : strict equality oprator, it doesnot do type coercion
@@ -245,6 +245,34 @@ const favourites =  Number(prompt("What's your favourite number?"));
 console.log(favourites);//the number input will be stored in the form of number
 console.log(typeof favourites)
 if (favourite !== 23) console.log('Why not 23?');//!== : strict, != : loose (different operator)
+*/
 
+//BOOLEAN LOGIC------------------------------------------------------------------------------------
+/* */
+//'!' not operator
+//not operator has higher presidence over 'or' and 'and' operator
+
+//LOGICAL OPERATORS--------------------------------------------------------------------------------
+/**/
+const hasDriversLicense = true;// A
+const hasGoodVision = false;// B
+
+console.log(hasDriversLicense && hasGoodVision);
+console.log(hasDriversLicense || hasGoodVision);
+console.log(!hasDriversLicense);
+
+if(hasDriversLicense && hasGoodVision) {
+  console.log('Sarah is able to drive!');
+}else{
+  console.log('Someone else should drive...');
+}
+
+const isTired = true; // C
+console.log(hasDriversLicense || hasGoodVision || isTired);
+if(hasDriversLicense && hasGoodVision && !isTired) { // use of not operator
+  console.log('Sarah is able to drive!');
+}else{
+  console.log('Someone else should drive...');
+}
 
 
