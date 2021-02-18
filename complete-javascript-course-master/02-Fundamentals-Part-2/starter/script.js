9a1380c0a673037684850325037521d9e800e174
Gaurab-Sarkar-au8 <64783626+Gaurab-Sarkar-au8@users.noreply.github.com>
2021-02-18 17:18:39 +0530

    strict mode

---
+++ complete-javascript-course-master/02-Fundamentals-Part-2/starter/script.js
@@ -0,0 +1,16 @@
+//ACTIVATING STRICT MODE---------------------------------------------------------------------------
+/* */
+//always write it at the top of the code
+//we can also use it for specific code blocks
+'use strict';
+//1.strict mode forbids us to do certain things  2.it creates visible errors in certain situations in which without strict mode javascript will silently fail without letting us know thst we did a mistake
+let hasDriversLicense = false;
+const passTest = true;
+
+if (passTest) hasDriverLicense = true;
+if (hasDriversLicense) console.log('I can drive :D');
+
+//shows reserved words by javascript
+const interface = 'Audio';
+const private = 534;
+//interfcae, private both words are reserved by javacript for future use
