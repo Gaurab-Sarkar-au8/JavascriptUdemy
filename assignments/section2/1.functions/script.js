d9b8fe86e3b5799d47caf6b6d9cdc2b370157395
Gaurab-Sarkar-au8 <64783626+Gaurab-Sarkar-au8@users.noreply.github.com>
2021-02-21 16:04:41 +0530

    no.1

---
+++ assignments/section2/1.functions/script.js
@@ -0,0 +1,10 @@
+function describeCountry(country, population, capitalCity) {
+  return `${country} has ${population} million people and its capital city is ${capitalCity}`
+}
+
+const country1 = describeCountry('India', 100, 'Delhi');
+console.log(country1)
+const country2 = describeCountry('Thailand', 100, 'Bangkok');
+console.log(country2)
+const country3 = describeCountry('Afganistan', 100, 'Kabul');
+console.log(country3)
