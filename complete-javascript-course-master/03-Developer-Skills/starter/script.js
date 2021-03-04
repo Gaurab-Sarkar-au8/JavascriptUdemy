4465101bd984417fb3aa0ac0dce4af23df8c6194
Gaurab-Sarkar-au8 <64783626+Gaurab-Sarkar-au8@users.noreply.github.com>
2021-03-04 12:06:56 +0530

    prettier

---
+++ complete-javascript-course-master/03-Developer-Skills/starter/script.js
@@ -0,0 +1,23 @@
+// Remember, we're gonna use strict mode in all scripts now!
+'use strict';
+
+//SETTING UP PRETTIER AND VS CODE-------------------------------------------------------------------
+/*
+//PRETTIER----------------
+//prettier is an opienated code formatter, which means it makes assumptions about how good code should look like, so if we use it the code will be nicely formatted
+
+//after installing prettier, second step is to define prettier as the default formatter of our code
+
+const x = '27';
+if (x == 27) console.log(x);
+
+//DOCS: https://prettier.io/docs/en/options.html (for editing prettier)
+//easiest way to configure pretier is to create a configuration file in the current project folder '.prettierrc'
+
+//SNIPPETS---------------
+//files => prferences => user snippets => new global snippets file => then enter the 'name' for the file
+console.log();
+*/
+
+//INSTALLING NODE JS AND SETTING UP A DEV ENVIRONMENT-----------------------------------------------
+/**/
