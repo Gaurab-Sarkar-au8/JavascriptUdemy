1e1f03721422c3491904a070a245b2dcd7a4df64
Gaurab-Sarkar-au8 <64783626+Gaurab-Sarkar-au8@users.noreply.github.com>
2021-03-04 18:04:30 +0530

    debugging

--- complete-javascript-course-master/03-Developer-Skills/starter/script.js
+++ complete-javascript-course-master/03-Developer-Skills/starter/script.js
@@ -33,7 +33,7 @@ console.log(`javascript is funny`);
 */
 
 //USING GOOGLE, StackOverflow and MDN--------------------------------------------------------------
-
+/*
 // PROBLEM 1:
 // We work for a company building a smart home thermometer. Our most recent task is this: "Given an array of temperatures of one day, calculate the temperature amplitude. Keep in mind that sometimes there might be a sensor error."
 
@@ -94,3 +94,56 @@ const calcTempAmplitudeNew = function (t1, t2) {
 };
 const amplitudeNew = calcTempAmplitudeNew(temperatures, [100, 200, 50]);
 console.log(amplitudeNew);
+*/
+//DEBUGGING WITH THE CONSOLE AND BREAKPOINTS---------------------------------------------------
+//First using console---
+const measureKelvin = function () {
+  const measurement = {
+    type: 'temp',
+    unit: 'celcius',
+
+    // C) FIX
+    // value: Number(prompt('Degree Celcius')),
+    // value: prompt('Degree Celcius'),
+
+    value: 10,
+  };
+
+  // B) FIND
+  console.log(measurement);
+  console.table(measurement);
+
+  // console.log(measurement.value);
+  // console.warn(measurement.value);
+  // console.error(measurement.value);
+
+  const kelvin = measurement.value + 273;
+  return kelvin;
+};
+// A) IDENTIFY
+console.log(measureKelvin());
+
+//Second using debugger in google chrome---
+//open console, click 'Sources' tab on right side, then press 'Page' and then 'script.js' on left side
+const temperatures = [3, -2, -6, -1, 'error', 9, 13, 17, 15, 14, 9, 5];
+const calcTempAmplitudeBug = function (t1, t2) {
+  let temps = t1.concat(t2);
+  console.log(temps);
+
+  let max = 0;
+  let min = 0;
+
+  for (let i = 0; i < temps.length; i++) {
+    let curTemp = temps[i];
+    if (typeof curTemp !== 'number') continue;
+
+    debugger;
+    if (curTemp > max) max = curTemp;
+    if (curTemp < min) min = curTemp;
+  }
+  console.log(min, max);
+  return max - min;
+};
+const amplitudeBug = calcTempAmplitudeBug(temperatures, [100, 200, 50]);
+// A) IDENTIFY
+console.log(amplitudeBug);
